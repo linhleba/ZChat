ee663b4521d6ffd2a9d7b4eca58f10700cecea26
BaLinh <linhleba.it@gmail.com>
2020-09-19 16:35:56 +0700

    create register form and submit

--- app/src/main/java/com/example/zchat/RegisterActivity.java
+++ app/src/main/java/com/example/zchat/RegisterActivity.java
@@ -1,14 +1,76 @@
 package com.example.zchat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private TextInputLayout mDisplayName;
+    private TextInputLayout mEmail;
+    private TextInputLayout mPassword;
+    private Button mCreateBtn;
+
+    // Firebase Authentication
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+        // Declare display, email and password
+        mDisplayName = (TextInputLayout) findViewById(R.id.res_name_text);
+        mEmail = (TextInputLayout) findViewById(R.id.res_email_text);
+        mPassword = (TextInputLayout) findViewById(R.id.res_password_text);
+        mCreateBtn = (Button) findViewById(R.id.res_btn);
+
+        mCreateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String display_name = mDisplayName.getEditText().getText().toString();
+                String email = mEmail.getEditText().getText().toString();
+                String password = mPassword.getEditText().getText().toString();
+
+                register_user(display_name, email, password);
+            }
+        }); 
+    }
+
+    private void register_user(String display_name, String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            //Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/zchat/StartActivity.java
+++ app/src/main/java/com/example/zchat/StartActivity.java
@@ -7,13 +7,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class StartActivity extends AppCompatActivity {
     private Button registerButton;
-
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        mAuth = FirebaseAuth.getInstance();
         registerButton = (Button) findViewById(R.id.start_reg_btn);
 
         registerButton.setOnClickListener(new View.OnClickListener() {
@@ -21,6 +24,7 @@ public class StartActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent reg_intent = new Intent(StartActivity.this, RegisterActivity.class);
                 startActivity(reg_intent);
+                //finish();
             }
         });
 
