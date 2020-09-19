4c30dc12962342be093fe73782ace4c034ceb776
BaLinh <linhleba.it@gmail.com>
2020-09-19 11:00:52 +0700

    create regis activy

--- app/src/main/java/com/example/zchat/StartActivity.java
+++ app/src/main/java/com/example/zchat/StartActivity.java
@@ -2,13 +2,28 @@ package com.example.zchat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
+    private Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        registerButton = (Button) findViewById(R.id.start_reg_btn);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent reg_intent = new Intent(StartActivity.this, RegisterActivity.class);
+                startActivity(reg_intent);
+            }
+        });
+
     }
+
 }
